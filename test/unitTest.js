be96391132a4dc38756ab113499fc954bc2cfdd5
Terry Chen <terrychen@Terrys-MacBook-Pro.local>
2014-11-09 21:30:30 -0800

    add test ui

--- test/unitTest.js
+++ test/unitTest.js
@@ -1,24 +1,12 @@
 var config 		= require('./unitTest.json'),
 	testCases 	= require('./testCases.json'),
+	colors		= require('colors'),
 	numTests	= testCases.length;
+	dashLine	= "------------------------------------------------------------------------------------------------------------------------------------".bgWhite;
+	asterisk	= "************************************************************************************************************************************".bgBlack;
+	pass 		= "PASS".green.bold;
+	fail 		= "FAIL".bgRed.white.bold;
 
-
-function capatilzieString( string ){
-
-	if (string){
-    	string = string.split(" ");
-    	string.forEach(function(element, index, array){
-			if( element[0] ){
-				array[index] = element && element[0].toUpperCase() + element.slice(1);
-			}
-	    });
-		var captedString = string.join(" ");
-		return captedString;
-	}
-	else {
-		return null;
-	}
-}
 
 function testCaseManager( config ){
 
@@ -36,139 +24,233 @@ function testCaseManager( config ){
 	return conf;
 }
 
+function printTestResult(numTest, numPass, numFails, failId, testName){
+
+	console.log("")
+	console.log(( "<< " + testName + " Result >> ").bold.blue);
+	console.log(("   Num Tests Ran: " + numTest).bold.blue);
+	console.log(("   Num Tests Failed: " + numFail).bold.blue);
+	console.log(("   Num Tests Passed: " + numPass).bold.blue);
+	console.log(("   Pass Rate: " + ((numPass/numTest)*100).toFixed(1) + "%").bold.blue);
+	console.log(("   Failed Ids: { " + failId + " }").bold.blue);
+	console.log("");
+
+}
+
 function checkMessageTest( config, checkMessage ){
 
-	var test = testCaseManager(config); 
+	var test 	= testCaseManager(config),
+		numTest = 0;
+		numFail = 0,
+		numPass	= 0;
+		failId  = [];
+
+	console.log(asterisk);
+	console.log("*************************************************** STARTING CHECK MESSAGE TESTS ***************************************************".bgWhite);
+	console.log(asterisk);
 
 	for( var i = test.start; i < test.end; i++ ){
+
+		numTest++;
 
 		var a_isDriver = checkMessage.isDriver(testCases[i].message);
 
 		if (a_isDriver == testCases[i].e_isDriver){
-			console.log("PASS");
+			numPass++;
+			console.log(pass);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("Expected: " + testCases[i].e_isDriver);
 			console.log("Actual: " + a_isDriver);
+			console.log(dashLine);
 		}
 		else{
-			console.log("FAIL");
+			numFail++;
+			failId.push(testCases[i].id);
+			console.log(fail);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("Expected: " + testCases[i].e_isDriver);
 			console.log("Actual: " + a_isDriver);
+			console.log(dashLine);
 		}
 	}
+
+	printTestResult(numTest, numPass, numFail, failId, "Check Message Test");
+
 }
 
 function originTest( config, locationTracker ){
 
-	var test = testCaseManager(config);
+	var test 	= testCaseManager(config),
+		numTest = 0;
+		numFail = 0,
+		numPass	= 0;
+		failId  = [];
+
+	console.log(asterisk);
+	console.log("************************************************** STARTING ORIGIN LOCATION TESTS **************************************************");
+	console.log(asterisk);
 
 	console.log("Staring Origin Test");
 
-		for( var i = test.start; i < test.end; i++ ){
+	for( var i = test.start; i < test.end; i++ ){
+
+		numTest++;
 
 		var location = locationTracker.searchLocation(testCases[i].message),
-			a_origin = capatilzieString(location.origin);
+			a_origin = location.origin;
 
 		if (a_origin == testCases[i].e_origin){
-			console.log("------------------------------------------------------------------------------------------------------------------------------");
-			console.log("PASS");
+			numPass++;
+			console.log(pass);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("Expected: " + testCases[i].e_origin);
 			console.log("Actual: " + a_origin);
+			console.log(dashLine);
 		}
 		else{
-			console.log("------------------------------------------------------------------------------------------------------------------------------");
-			console.log("FAIL");
+			numFail++;
+			failId.push(testCases[i].id);
+			console.log(fail);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("Expected: " + testCases[i].e_origin);
 			console.log("Actual: " + a_origin);
+			console.log(dashLine);
 		}
 	}
+
+	printTestResult(numTest, numPass, numFail, failId, "Origin Location Test");
 }
 
 
 function destinationTest( config, locationTracker ){
 
-	var test = testCaseManager(config);
+	var test 	= testCaseManager(config),
+		numTest = 0;
+		numFail = 0,
+		numPass	= 0;
+		failId  = [];
 
-		for( var i = test.start; i < test.end; i++ ){
+		console.log(asterisk);
+		console.log("*********************************************** STARTING DESTINATION LOCATION TESTS ************************************************");
+		console.log(asterisk);
+
+	for( var i = test.start; i < test.end; i++ ){
+
+		numTest++;
 
 		var location = locationTracker.searchLocation(testCases[i].message),
-			a_destination = capatilzieString(location.destination);
+			a_destination = location.destination;
 
 		if (a_destination == testCases[i].e_destination){
-			console.log("PASS");
+			numPass++;
+			console.log(pass);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("Expected: " + testCases[i].e_destination);
 			console.log("Actual: " + a_destination);
+			console.log(dashLine);
 		}
 		else{
-			console.log("FAIL");
+			numFail++;
+			failId.push(testCases[i].id);
+			console.log(fail);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("Expected: " + testCases[i].e_destination);
 			console.log("Actual: " + a_destination);
+			console.log(dashLine);
 		}
 	}
+	printTestResult(numTest, numPass, numFail, failId, "Destination Location Test");
 }
 
 function departureTest( config, timeTracker ){
 
-	var test = testCaseManager(config);
+	var test 	= testCaseManager(config),
+		numTest = 0;
+		numFail = 0,
+		numPass	= 0;
+		failId  = [];
 
-		for( var i = test.start; i < test.end; i++ ){
+	console.log(asterisk);
+	console.log("*************************************************** STARTING DEPARTURE TIME TESTS **************************************************");
+	console.log(asterisk);
+
+	for( var i = test.start; i < test.end; i++ ) {
+
+		numTest++;
 
 		var a_departure = timeTracker.extractDeparture(testCases[i].message, testCases[i].timestamp);
 
 		if (a_departure == testCases[i].e_departure){
-			console.log("PASS");
+			numPass++;
+			console.log(pass);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("TimeStamp: " + testCases[i].timestamp)
 			console.log("Expected: " + testCases[i].e_departure);
 			console.log("Actual: " + a_departure);
+			console.log(dashLine);
 		}
 		else{
-			console.log("FAIL");
+			numFail++;
+			failId.push(testCases[i].id);
+			console.log(fail);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("TimeStamp: " + testCases[i].timestamp)
 			console.log("Expected: " + testCases[i].e_departure);
 			console.log("Actual: " + a_departure);
+			console.log(dashLine);
 		}
 	}
+	printTestResult(numTest, numPass, numFail, failId, "Departure Time Test");
 }
 
 function priceTest( config, priceTracker ){
 
-	var test = testCaseManager(config);
+	var test 	= testCaseManager(config),
+		numTest = 0;
+		numFail = 0,
+		numPass	= 0;
+		failId  = [];
 
-		for( var i = test.start; i < test.end; i++ ){
+	console.log(asterisk);
+	console.log("**************************************************** STARTING PRICE CHECK TESTS ****************************************************");
+	console.log(asterisk);
+
+	for( var i = test.start; i < test.end; i++ ){
+
+		numTest++;
 
 		var priceCheck = priceTracker.searchPrice(testCases[i].message),
 			a_price = priceCheck.price;
 
 		if (a_price == testCases[i].e_price){
-			console.log("PASS");
+			numPass++;
+			console.log(pass);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("Expected: " + testCases[i].e_price);
 			console.log("Actual: " + a_price);
+			console.log(dashLine);
 		}
 		else{
-			console.log("FAIL");
+			numFail++;
+			failId.push(testCases[i].id);
+			console.log(fail);
 			console.log("id: " + testCases[i].id);
 			console.log("Message: " + testCases[i].message);
 			console.log("Expected: " + testCases[i].e_price);
 			console.log("Actual: " + a_price);
+			console.log(dashLine);
 		}
 	}
+	printTestResult(numTest, numPass, numFail, failId, "Price Check Test");
 }
 
 function capacityTest( config ){
