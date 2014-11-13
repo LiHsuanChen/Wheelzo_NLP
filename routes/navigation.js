6da27948dd80c66aa888b42d73943ad904a1325e
Terry Chen <terrychen@Terrys-MacBook-Pro.local>
2014-11-12 23:47:41 -0800

    debuging

--- routes/navigation.js
+++ routes/navigation.js
@@ -68,7 +68,11 @@ exports.restTest = function(req, res){
  exports.nlpApi = function(req, res){
 
 console.log("C-TYPE: " + req.get('content-type'))
-console.log("BODY: " + req.body);
+for (var key in req.body){
+  var obj = req.body[key];
+    console.log(key + " = " + obj);
+  }
+//console.log("BODY: " + req.body);
 
   var request = req.body,
       api = new apiResponse();
